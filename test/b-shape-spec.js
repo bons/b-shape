7b54af60889371b248d1f98f8b1b86ff47d99dc6
Juan <beogip@gmail.com>
2015-04-22 14:41:13 -0300

    only a share commit

---
+++ test/b-shape-spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+require('angular');
+require('angular-mocks');
+var app = require('../lib/b-shape');
+
+describe('Test Suite: bShape', function()
+{
+  var scope,
+      $compile;
+
+  function injectHTML()
+  {
+    var body  = document.querySelector("body");
+    body.innerHTML = '<canvas b-shape type="circle" width="100px" height="100px"></canvas>';
+
+    $compile(body)(scope);
+
+    return body.querySelector("[b-shape]");
+  }
+
+  beforeEach(angular.mock.module('bons.bShape'));
+
+  beforeEach(angular.mock.inject(['$rootScope','$compile',
+      function ($rootScope, _$compile)
+      {
+        scope = $rootScope.$new();
+        $compile = _$compile;
+      }
+    ])
+  );
+
+  it('should be defined', function()
+  {
+    expect(app).toBeDefined();
+  });
+
+  it('', function()
+  {
+    injectHTML();
+
+  });
+});
